6436e27ba082877049cec37de8e5eec8b37f7f9a
Antoine Park <asuse35@hotmail.fr>
2025-03-13 11:09:29 +0100

    WIP

--- Assets/PlayerMoverment.cs
+++ Assets/PlayerMoverment.cs
@@ -4,31 +4,44 @@ using UnityEngine.InputSystem;
 public class PlayerMoverment : MonoBehaviour
 {
     public Rigidbody2D rb;
+    [Header("Movement")]
     public float moveSpeed = 5f;
-    public float jumpForce = 10f; // Augmentation de la force de saut
-    private bool isGrounded = true; // Vérification si le joueur est au sol
     float horizontalMovement;
+    [Header("Jump")]
+    public float jumpForce = 10f; // Augmentation de la force de saut
+    public int maxJumps = 2; // Nombre de saut maximum
+    int jumpsRemaining; // Nombre de saut restant
+    [Header("GroundCheck")]
+    public Transform groundCheckPos;
+    public Vector2 groundCheckSize = new Vector2(0.5f, 0.05f);
+    public LayerMask groundLayer;
+    [Header("Gravity")]
+    public float baseGravity = 2f;
+    public float maxFallSpeed = 18f;
+    public float fallSpeedMultiplier = 2f; 
+    [Header("Animator")]
     public Animator animator;
-
-    // Start is called une fois avant la première exécution de Update après la création du MonoBehaviour
-    void Start()
-    {
-        
-    }
 
     // Update est appelé une fois par frame
     void Update()
     {
         rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
-
-        // Vérification si le joueur appuie sur la touche Espace et s'il est au sol
-        if (Keyboard.current.spaceKey.wasPressedThisFrame && isGrounded)
-        {
-            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-            isGrounded = false; // Le joueur n'est plus au sol après avoir sauté
-        }
-
         animator.SetFloat("magnitude", rb.linearVelocity.magnitude);
+        GroundCheck();
+        Gravity();
+    }
+
+    private void Gravity()
+    {
+        if(rb.velocity.y < 0)
+        {
+            rb.gravityScale = baseGravity * fallSpeedMultiplier;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, -maxFallSpeed));
+        }
+        else
+        {
+            rb.gravityScale = baseGravity;
+        }
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -36,12 +49,35 @@ public class PlayerMoverment : MonoBehaviour
         horizontalMovement = context.ReadValue<Vector2>().x;
     }
 
-    // Méthode pour détecter la collision avec le sol
-    private void OnCollisionEnter2D(Collision2D collision)
+    public void Jump(InputAction.CallbackContext context)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if(jumpsRemaining > 0)
         {
-            isGrounded = true; // Le joueur est de nouveau au sol
+            if (context.performed)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+                jumpsRemaining--;
+            }
+            else if (context.canceled)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+                jumpsRemaining--;
+            }
         }
+    }
+
+    private void GroundCheck()
+    {
+        if(Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
+        {
+            jumpsRemaining = maxJumps;
+        }
+    }
+
+    // Méthode pour détecter la collision avec le sol
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
     }
 }
